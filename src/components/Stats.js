fad5d53e25ee1be693d8136205215c7d960e4c50
Christian Vari <19372852+christianvari@users.noreply.github.com>
2025-02-05 00:18:05 +0100

    (feat) real stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,6 +4,19 @@ import { useStaticQuery, graphql } from "gatsby";
 const Stats = () => {
   const data = useStaticQuery(graphql`
     query {
+      allAuditHistoryJson {
+        edges {
+          node {
+            id
+            issues {
+              critical
+              major
+              minor
+              informational
+            }
+          }
+        }
+      }
       allSrcJson {
         edges {
           node {
@@ -19,12 +32,32 @@ const Stats = () => {
     }
   `);
 
-  const newData = data.allSrcJson.edges[0].node.banner.stats;
   return (
     <div className="stats">
       <div className="container">
         <div className="row">
-          {newData?.map((data, i) => (
+          <div className="col-xl-3 col-6">
+            <h2 className="stats-heading">
+              {data.allAuditHistoryJson.edges.length}
+            </h2>
+            <p className="stats-text">Completed Audits</p>
+          </div>
+          <div className="col-xl-3 col-6">
+            <h2 className="stats-heading">
+              {data.allAuditHistoryJson.edges.reduce((p, n) => {
+                if (!n.node.issues) return p;
+                return (
+                  p +
+                  n.node.issues.critical +
+                  n.node.issues.major +
+                  n.node.issues.minor +
+                  n.node.issues.informational
+                );
+              }, 0)}
+            </h2>
+            <p className="stats-text">Vulnerabilities Identified</p>
+          </div>
+          {data.allSrcJson.edges[0].node.banner.stats?.map((data, i) => (
             <div className="col-xl-3 col-6" key={i}>
               <h2 className="stats-heading">{data.number}</h2>
               <p className="stats-text">{data.description}</p>
