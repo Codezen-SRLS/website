f30476714e2e424b2a1e25cbc72c5e6fa43b8100
Christian Vari <19372852+christianvari@users.noreply.github.com>
2025-04-14 17:24:51 +0200

    (feat) update deps

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,52 +6,12 @@
  */
 
 import * as React from "react";
-import { useState, useEffect } from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
 
 import Header from "./header";
 import "./layout.css";
 import Footer from "./Footer";
-import { ThemeProvider, useTheme } from "../context/ThemeContext";
-
-// Inner layout component that can use the theme context
-const InnerLayout = ({ children, siteTitle }) => {
-  const { isDarkMode } = useTheme();
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  return (
-    <>
-      {isClient && isDarkMode ? (
-        <StaticImage
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            minHeight: "100%",
-            zIndex: -1,
-            objectFit: "fill",
-          }}
-          layout="fullWidth"
-          alt="background"
-          src={"../images/bg.png"}
-          formats={["auto", "webp", "avif"]}
-          placeholder="blurred"
-          backgroundColor="#131313"
-        />
-      ) : null}
-
-      <Header siteTitle={siteTitle} />
-      {children}
-      <Footer />
-    </>
-  );
-};
+import { ThemeProvider } from "../context/ThemeContext";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -66,9 +26,9 @@ const Layout = ({ children }) => {
 
   return (
     <ThemeProvider>
-      <InnerLayout siteTitle={data.site.siteMetadata?.title || `Title`}>
-        {children}
-      </InnerLayout>
+      <Header siteTitle={data.site.siteMetadata?.title} />
+      {children}
+      <Footer />
     </ThemeProvider>
   );
 };
